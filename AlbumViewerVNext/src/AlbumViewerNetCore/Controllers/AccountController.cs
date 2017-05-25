ba14d2c1b30aee717eff2dae8cf3fc39a26eacae
Tuan Pham <phamtuanchip@gmail.com>
2017-05-26 01:45:13 +0700

    add example

---
+++ AlbumViewerVNext/src/AlbumViewerNetCore/Controllers/AccountController.cs
@@ -0,0 +1,63 @@
+﻿using AlbumViewerBusiness;
+using System.Threading.Tasks;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Cors;
+
+namespace AlbumViewerAspNetCore
+{    
+    [ServiceFilter(typeof(ApiExceptionFilter))]    
+    [EnableCors("CorsPolicy")]
+    public class AccountController : Controller
+    {
+        private AccountRepository accountRepo;
+
+        public AccountController(AccountRepository actRepo)            
+        {
+            accountRepo = actRepo;
+        }
+
+           
+        [AllowAnonymous]                    
+        [HttpPost]
+        [Route("api/login")]
+        public async Task<bool> Login([FromBody]  User loginUser)
+        {            
+            var user = await accountRepo.AuthenticateAndLoadUser(loginUser.Username, loginUser.Password);
+
+            if (user == null)
+                throw new ApiException("Invalid Login Credentials", 401);
+
+
+            var identity = new ClaimsIdentity(CookieAuthenticationDefaults.AuthenticationScheme);
+            identity.AddClaim(new Claim(ClaimTypes.Name, user.Username))    ;
+           
+            if (user.Fullname == null)
+                user.Fullname = string.Empty;
+            identity.AddClaim(new Claim("FullName", user.Fullname));
+
+            await HttpContext.Authentication.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme,
+                new ClaimsPrincipal(identity));
+
+            return true;
+        }
+
+        [AllowAnonymous]
+        [HttpGet]
+        [Route("api/logout")]
+        public async Task<bool> Logout()
+        {
+            await HttpContext.Authentication.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);            
+            return true;
+        }
+
+        [HttpGet]
+        [Route("api/isAuthenticated")]
+        public bool IsAuthenthenticated()
+        {
+            return User.Identity.IsAuthenticated;
+        }
+    }
+}
---
+++ AlbumViewerVNext/src/AlbumViewerNetCore/Startup.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using AlbumViewerBusiness;
+using Microsoft.EntityFrameworkCore;
+using System.IO;
+using System.Text.Encodings.Web;
+using AlbumViewerAspNetCore;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json.Serialization;
+using Serilog;
+
+namespace AlbumViewerNetCore
+{
+    public class Startup
+    {
+        readonly IHostingEnvironment HostingEnvironment;
+
+        IConfigurationRoot Configuration { get; }
+
+        public Startup(IHostingEnvironment env)
+        {
+            HostingEnvironment = env;
+            
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
+        }
+
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<AlbumViewerContext>(builder =>
+            {
+                string useSqLite = Configuration["Data:useSqLite"];
+                if (useSqLite != "true")
+                {
+                    var connStr = Configuration["Data:SqlServerConnectionString"];
+                    builder.UseSqlServer(connStr);
+                }
+                else
+                {
+                    // Note this path has to have full  access for the Web user in order 
+                    // to create the DB and write to it.
+                    var connStr = "Data Source=" +
+                                  Path.Combine(HostingEnvironment.ContentRootPath, "AlbumViewerData.sqlite");
+                    builder.UseSqlite(connStr);
+                }
+            });
+
+            services.AddAuthentication();
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder
+                        .AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader()
+                        .AllowCredentials());
+            });
+
+            // Make configuration available for EF configuration
+            services.AddSingleton<IConfigurationRoot>(Configuration);
+            services.AddSingleton<IConfiguration>(Configuration);
+
+			         // Instance injection
+            services.AddTransient<AlbumRepository>();
+            services.AddTransient<ArtistRepository>();
+            services.AddTransient<AccountRepository>();
+
+            //Log.Logger = new LoggerConfiguration()
+            //        .WriteTo.RollingFile(pathFormat: "logs\\log-{Date}.log")
+            //        .CreateLogger();
+            //services.AddSingleton(Log.Logger);
+
+												// Per request injections
+            services.AddScoped<ApiExceptionFilter>();
+
+            // Add framework services
+            services.AddMvc(options =>
+                {
+                    // options.Filters.Add(new ApiExceptionFilter());
+                })
+            .AddJsonOptions(opt =>
+            {
+                var resolver = opt.SerializerSettings.ContractResolver;
+                if (resolver != null)
+                {
+                    var res = resolver as DefaultContractResolver;
+                    res.NamingStrategy = null;
+                }
+            });            
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, 
+            IHostingEnvironment env, 
+            ILoggerFactory loggerFactory,
+            AlbumViewerContext albumContext,
+            IConfiguration configuration)
+        {
+
+            Log.Logger = new LoggerConfiguration()
+                    .WriteTo.RollingFile(pathFormat: "logs\\log-{Date}.log")
+                    .CreateLogger();
+
+            if (env.IsDevelopment())
+            {
+                loggerFactory.WithFilter(new FilterLoggerSettings
+                    {
+                        {"Trace", LogLevel.Trace},
+                        {"Default", LogLevel.Trace},
+                        {"Microsoft", LogLevel.Warning}, // very verbose
+                        {"System", LogLevel.Warning}
+                    })
+                    .AddConsole()
+                    .AddSerilog();
+
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                loggerFactory.WithFilter(new FilterLoggerSettings
+                    {
+                        {"Trace", LogLevel.Trace},
+                        {"Default", LogLevel.Trace},
+                        {"Microsoft", LogLevel.Warning}, // very verbose
+                        {"System", LogLevel.Warning}
+                    })
+                    .AddSerilog();
+
+                app.UseExceptionHandler(errorApp =>
+
+                    // Application level exception handler here - this is just a place holder
+                        errorApp.Run(async (context) =>
+                        {
+                            context.Response.StatusCode = 500;
+                            context.Response.ContentType = "text/html";
+                            await context.Response.WriteAsync("<html><body>\r\n");
+                            await context.Response.WriteAsync(
+                                    "We're sorry, we encountered an un-expected issue with your application.<br>\r\n");
+
+                            // Capture the exception
+                            var error = context.Features.Get<IExceptionHandlerFeature>();
+                            if (error != null)
+                            {
+                                // This error would not normally be exposed to the client
+                                await
+                                    context.Response.WriteAsync("<br>Error: " +
+                                                                HtmlEncoder.Default.Encode(error.Error.Message) +
+                                                                "<br>\r\n");
+                            }
+                            await context.Response.WriteAsync("<br><a href=\"/\">Home</a><br>\r\n");
+                            await context.Response.WriteAsync("</body></html>\r\n");
+                            await context.Response.WriteAsync(new string(' ', 512)); // Padding for IE
+                        }));
+
+                //loggerFactory.AddConsole();
+                //app.UseExceptionHandler("/");
+                //app.UseExceptionHandler("/Home/Error");
+            }
+
+            Console.WriteLine("\r\nPlatform: " + System.Runtime.InteropServices.RuntimeInformation.OSDescription);
+												string useSqLite = Configuration["Data:useSqLite"];
+												Console.WriteLine(useSqLite == "true" ? "SqLite" : "Sql Server");
+				
+
+            // Enable Cookie Auth with automatic user policy
+            app.UseCookieAuthentication(new CookieAuthenticationOptions()
+            {
+                AutomaticAuthenticate = true,
+                AutomaticChallenge = false,
+                LoginPath = "/api/login"
+            });
+
+            app.UseDatabaseErrorPage();
+            app.UseStatusCodePages();
+
+            app.UseDefaultFiles(); // so index.html is not required
+            app.UseStaticFiles();
+
+            // put last so header configs like CORS or Cookies etc can fire
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+
+            AlbumViewerDataImporter.EnsureAlbumData(albumContext,
+                Path.Combine(env.ContentRootPath, "albums.js"));
+
+        }
+    }
+}
+
